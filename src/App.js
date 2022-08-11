ba091c77cc24fe87a259948664cc7caf2af1d0dc
muxtarHesenov <muxtar.h6@gmail.com>
2022-08-11 20:13:52 +0400

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text:"",
+      todoList:[]
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+  handleInputChange(e) {
+    this.setState ({
+      text:e.target.value
+    })
+  }
+  handleFormSubmit(e) {
+    e.preventDefault();
+    const {text, todoList} = this.state;
+    const newTodoList = [...todoList, text];
+    this.setState ({
+      todoList:newTodoList,
+      text:""
+    })
+  }
+  handleDelete(e) {
+    e.target.className ="d-none"
+  }
+  render() {
+    const {text, todoList} = this.state;
+    return (
+      <>
+      <div onSubmit={this.handleFormSubmit} className="container mb-3">
+        <form className="mb-3 ">
+          <label 
+          htmlFor="todo" 
+          className="form-label">
+            Todo title
+          </label>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <input 
+          type="text" 
+          className="form-control" 
+          id="todo" 
+          placeholder="text"
+          value={text}
+          onChange={this.handleInputChange}/>
+        </form>
+        <ul className="list-group">
+          {
+            todoList.map(todoItem => (
+              <li onClick={this.handleDelete} className="list-group-item">{todoItem}</li>
+            ))
+          }
+        </ul>
+      </div>
+      </>
+    );
+  }
 }
-
-export default App;
